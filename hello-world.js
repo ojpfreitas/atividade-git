6d32df8d6127cd6a96628ebecf75af4fb2043b28
Laboratório 05 <lab05@esucri.edu>
2025-03-24 20:53:28 -0300

    atividade realizada

--- hello-world.js
+++ hello-world.js
@@ -5,8 +5,28 @@ nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
     }
+
+    if (event.key === "Enter"){
+        helloWorld();
+    }
+
+    if (event.key === "Escape"){
+        nameInput.value = '';
+        messageP.innerText = '';
+    }
+
 }
 
 const helloWorld = () => {
-    messageP.innerText = `Hello World, ${nameInput.value}!`;
+    const name = nameInput.value.trim();
+    if (name){
+        messageP.innerText = `Hello World, ${name}!`;    
+    } else {
+        messageP.innerText = `Hello World!`;
+    }
+}
+
+const limpar = () =>{
+    nameInput.value = '';
+    messageP.innerText = '';
 }
